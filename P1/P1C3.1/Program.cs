83b191d185497e8819cc0d60a713e6f4c490cf43
Caro639 <120673222+Caro639@users.noreply.github.com>
2026-02-11 21:44:35 +0100

    add files + exercices

---
+++ P1/P1C3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿int numerateur = 10;
+int denominateur = 4;
+
+// TODO : modifier l'instruction ci-dessous pour que la réponse prenne une valeur décimale
+// int reponse = numerateur / denominateur;
+// double reponse = (double)numerateur / denominateur;
+double reponse = numerateur / (double)denominateur;
+
+// Afficher le résultat 
+Console.WriteLine(numerateur + " / " + denominateur + " = " + reponse);
---
+++ P1/P1C4/Program.cs
@@ -0,0 +1,13 @@
+﻿using P1C4;
+// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
+Livre monLivre = new Livre();
+
+// TODO, étape 2 : affecter une valeur aux champs titre, auteur et nombre de pages de votre objet
+monLivre.Titre = "Le Petit Prince";
+monLivre.Auteur = "Antoine de Saint-Exupéry";
+monLivre.NombreDePages = 96;
+
+// Afficher le contenu des champs de cet objet 
+Console.WriteLine("Le titre du livre est " + monLivre.Titre);
+Console.WriteLine("Son auteur est " + monLivre.Auteur);
+Console.WriteLine("Il compte " + monLivre.NombreDePages + " pages.");
