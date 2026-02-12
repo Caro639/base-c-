b85ac67d4bdf40fc30cc0b4398f5419d224a0b4f
Caro639 <120673222+Caro639@users.noreply.github.com>
2026-02-12 18:22:30 +0100

    add exercice boucle

--- P2/P2C3/Program.cs
+++ P2/P2C3/Program.cs
@@ -1,1 +1,32 @@
 ﻿//TODO : Créer une boucle for et while qui affiche à 5 reprises la phrase, « Je m'amuse comme un fou ! ».
+using System; 
+namespace Exercice_1;
+
+ class Program 
+{ 
+    static void Main(string[] args)
+    {
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (i == 4)
+                {
+                    continue;
+                }
+            Console.WriteLine("Je m'amuse comme un fou !");
+        }
+
+
+        // string phraseBoucle = "Je m'amuse comme un fou !";
+
+        // int nombrePhrase = 0;
+
+        // while (nombrePhrase < 5) 
+        // { 
+        //     nombrePhrase += 1;
+
+        //     Console.WriteLine(phraseBoucle); 
+        // }
+        // Console.WriteLine("Fin de la boucle while");
+    }
+}
---
+++ P2C2.2-c#/Program.cs
@@ -0,0 +1,28 @@
+﻿using P2C2._2;
+
+Couleur couleurChoisie = Couleur.Vert;
+
+// Remplacer la séquence if/else par une instruction switch équivalente qui fonctionne selon la valeur de couleurChoisie
+    
+switch (couleurChoisie)
+{
+    case Couleur.Bleu:
+        Console.WriteLine("La couleur est bleue");
+        break;
+
+    case Couleur.Vert: 
+        Console.WriteLine("La couleur est verte"); 
+        break;
+
+    case Couleur.Rouge:
+        Console.WriteLine("La couleur est rouge");
+        break;
+
+    case Couleur.Jaune:
+        Console.WriteLine("La couleur est jaune");
+        break;
+
+    default:    
+    Console.WriteLine("Cette couleur n'est pas connue !");
+    break;
+}
