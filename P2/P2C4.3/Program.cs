161a2be173c4096855022be3b107d94bd087abac
Caro639 <120673222+Caro639@users.noreply.github.com>
2026-02-12 22:58:08 +0100

    add exercice error exception

--- P2/P2C4.3/Program.cs
+++ P2/P2C4.3/Program.cs
@@ -1,14 +1,29 @@
 ﻿using P2C4._3;
 
-List<int> temperaturesEnregistreDegresCelcius = new List<int>();
-
-// remplir la liste à partir des valeurs fournies comme arguments en ligne de commande
-foreach (string stringRepresentationTemperature in args)
+try
 {
-    int temperature = int.Parse(stringRepresentationTemperature);
-    temperaturesEnregistreDegresCelcius.Add(temperature);
-}
 
-// Calculer et afficher la température moyenne
-int moyenneTemperature = MathSimple.CalculMoyenne(temperaturesEnregistreDegresCelcius);
-Console.WriteLine("La température moyenne est " + moyenneTemperature);
+    // List<int> temperaturesEnregistreDegresCelcius = new List<int>();
+    List<int> temperaturesEnregistreDegresCelcius = [];
+
+    // remplir la liste à partir des valeurs fournies comme arguments en ligne de commande
+    foreach (string stringRepresentationTemperature in args)
+    {
+        int temperature = int.Parse(stringRepresentationTemperature);
+        temperaturesEnregistreDegresCelcius.Add(temperature);
+    }
+
+    // Calculer et afficher la température moyenne
+    int moyenneTemperature = MathSimple.CalculMoyenne(temperaturesEnregistreDegresCelcius);
+    Console.WriteLine("La température moyenne est " + moyenneTemperature);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Les températures fournies doivent être des nombres");
+    Environment.Exit(-1);
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Vous devez fournir au moins une température");
+    Environment.Exit(-1);
+}
